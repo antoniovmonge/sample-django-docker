b3108efa25a373fcd782c2a981a89ba29c15bfbf
Antonio Vilchez Monge <antoniovmonge@gmail.com>
2022-02-21 18:24:18 +0100

    new folder structure - .env.dev

--- app/project/project/settings.py
+++ app/project/project/settings.py
@@ -25,8 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False) in [True, "True", "1"]
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
+DEBUG = int(os.environ.get("DEBUG", default=0))
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
