3889152bb019e4efc0507c7cd07a3df08384ba97
Antonio Vilchez Monge <antoniovmonge@gmail.com>
2022-02-22 13:20:56 +0100

    test and model - Member

--- app/members/models.py
+++ app/members/models.py
@@ -1,7 +1,19 @@
-from django.db import models
+# app/members/models.py
 
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
     pass
+
+
+class Member(models.Model):
+    name = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    fun_fact = models.TextField(max_length=400)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
---
+++ app/tests/members/test_models.py
@@ -0,0 +1,28 @@
+# app/tests/members/test_models.py
+
+import pytest
+
+from members.models import Member
+
+
+@pytest.mark.django_db
+def test_member_model():
+    member = Member(
+        name="Phanos Hadjikyriakou",
+        position="Co-founder & CEO",
+        fun_fact="""
+            Obsessed with the sea, movement, cooking and working on changing the world!
+            """,
+    )
+    member.save()
+    assert member.name == "Phanos Hadjikyriakou"
+    assert member.position == "Co-founder & CEO"
+    assert (
+        member.fun_fact
+        == """
+            Obsessed with the sea, movement, cooking and working on changing the world!
+            """
+    )
+    assert member.created_date
+    assert member.updated_date
+    assert str(member) == member.name
