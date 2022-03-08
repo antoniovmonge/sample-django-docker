d929a0e2387bc8d665884e43a32f70d33be4c715
Antonio Vilchez Monge <antoniovmonge@gmail.com>
2022-03-08 10:02:37 +0100

    removed lastname from test

--- app/tests/members/test_serializers.py
+++ app/tests/members/test_serializers.py
@@ -3,7 +3,7 @@ from members.serializers import MemberSerializer
 
 def test_valid_member_serializer():
     valid_serializer_data = {
-        "name": "Phanos Hadjikyriakou",
+        "name": "Phanos",
         "position": "Co-founder & CEO",
         "fun_fact": "Obsessed with the sea, movement, cooking and working on changing the world!",
     }
@@ -16,7 +16,7 @@ def test_valid_member_serializer():
 
 def test_invalid_member_serializer():
     invalid_serializer_data = {
-        "name": "Phanos Hadjikyriakou",
+        "name": "Phanos",
         "position": "Co-founder & CEO",
     }
     serializer = MemberSerializer(data=invalid_serializer_data)
