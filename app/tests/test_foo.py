e9392848d487a4b603d0b0f197e010a9470fcd02
Antonio Vilchez Monge <antoniovmonge@gmail.com>
2022-03-08 08:53:30 +0100

    removed test hello-world

--- app/tests/test_foo.py
+++ app/tests/test_foo.py
@@ -3,11 +3,6 @@ import json
 from django.urls import reverse
 
 
-def test_hello_world():
-    assert "hello_world" == "hello_world"
-    assert "foo" != "bar"
-
-
 def test_ping(client):
     url = reverse("ping")
     response = client.get(url)
